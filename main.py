3a4fa79fe6c5aade624511bc681da4fbe961f6a6
Folarin <fatungasefolarin39@gmail.com>
2022-12-30 02:35:48 -0800

    fast-api initial commit

--- main.py
+++ main.py
@@ -7,19 +7,19 @@ from db.models import user, course
 user.Base.metadata.create_all(bind=engine)
 course.Base.metadata.create_all(bind=engine)
 
-
 app = FastAPI(
     title="Fast API LMS",
     description="LMS for managing students and courses.",
     version="0.0.1",
     contact={
-        "name": "Folarin",
-        "email": "fatungasefolarin39@gmail.com",
+        "name": "folarin",
+        "email": "fatungasefolarin39@example.com",
     },
     license_info={
         "name": "MIT",
-    }
+    },
 )
+
 app.include_router(users.router)
 app.include_router(courses.router)
 app.include_router(sections.router)
